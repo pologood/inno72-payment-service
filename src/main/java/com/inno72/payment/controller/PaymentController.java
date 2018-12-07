d1cbc612b0ccbcb93adc8a18c82ed10989ae24d1
周禹 <zhouyu@inno72.com>
2018-12-07 17:03:42 +0800

    fix

--- src/main/java/com/inno72/payment/controller/PaymentController.java
+++ src/main/java/com/inno72/payment/controller/PaymentController.java
@@ -7,11 +7,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -29,7 +28,7 @@ import com.inno72.payment.service.BillService;
 import com.inno72.payment.service.ChannelFactoryService;
 import com.inno72.payment.service.IdWorker;
 
-@Controller
+@RestController
 @RequestMapping("/pay")
 public class PaymentController {
 	
@@ -46,7 +45,6 @@ public class PaymentController {
 	
 	
 	@RequestMapping(value="/queryBillInfo")
-	@ResponseBody
     public  Result<RspQueryBillBean> queryBillInfo(@RequestBody ReqQueryBillBean reqBean) throws TransException {
 
 		return billService.queryBill(reqBean);
@@ -54,7 +52,6 @@ public class PaymentController {
     }
 	
 	@RequestMapping(value="/create", method = RequestMethod.POST)
-	@ResponseBody
 	public  Result<RspCreateBillBean> createBill(@RequestBody ReqCreateBillBean reqBean) throws TransException {
 		
 		logger.info(reqBean.toString());
