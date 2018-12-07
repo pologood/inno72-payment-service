40127f76ebca6351965e9624838fe18e4303fe72
周禹 <zhouyu@inno72.com>
2018-12-07 15:41:20 +0800

    fix

--- src/main/java/com/inno72/payment/controller/PaymentController.java
+++ src/main/java/com/inno72/payment/controller/PaymentController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -46,7 +45,7 @@ public class PaymentController {
 	
 	@RequestMapping("/queryBillInfo")
 	@ResponseBody
-    public  Result<RspQueryBillBean> queryBillInfo(@RequestBody ReqQueryBillBean reqBean) throws TransException {
+    public  Result<RspQueryBillBean> queryBillInfo(ReqQueryBillBean reqBean) throws TransException {
 
 		return billService.queryBill(reqBean);
 		
@@ -54,7 +53,7 @@ public class PaymentController {
 	
 	@RequestMapping("/create")
 	@ResponseBody
-	public  Result<RspCreateBillBean> createBill(@RequestBody ReqCreateBillBean reqBean) throws TransException {
+	public  Result<RspCreateBillBean> createBill(ReqCreateBillBean reqBean) throws TransException {
 		
 		logger.info(reqBean.toString());
 		
