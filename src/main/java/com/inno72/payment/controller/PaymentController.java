ddbcd35f4e264cf2f0de710fe63c6309a16439d9
周禹 <zhouyu@inno72.com>
2018-12-07 17:07:02 +0800

    fix

--- src/main/java/com/inno72/payment/controller/PaymentController.java
+++ src/main/java/com/inno72/payment/controller/PaymentController.java
@@ -7,9 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -45,14 +43,14 @@ public class PaymentController {
 	
 	
 	@RequestMapping(value="/queryBillInfo")
-    public  Result<RspQueryBillBean> queryBillInfo(@RequestBody ReqQueryBillBean reqBean) throws TransException {
+    public  Result<RspQueryBillBean> queryBillInfo(ReqQueryBillBean reqBean) throws TransException {
 
 		return billService.queryBill(reqBean);
 		
     }
 	
-	@RequestMapping(value="/create", method = RequestMethod.POST)
-	public  Result<RspCreateBillBean> createBill(@RequestBody ReqCreateBillBean reqBean) throws TransException {
+	@RequestMapping(value="/create")
+	public  Result<RspCreateBillBean> createBill(ReqCreateBillBean reqBean) throws TransException {
 		
 		logger.info(reqBean.toString());
 		
